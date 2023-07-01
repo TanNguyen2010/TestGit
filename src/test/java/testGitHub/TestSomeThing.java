1eae57875131c19f4d82b3fc0b98f15136f54212
thuanvo191 <thuanvo191@gmail.com>
2023-07-01 22:12:47 +0700

    commit all

--- src/test/java/testGitHub/TestSomeThing.java
+++ src/test/java/testGitHub/TestSomeThing.java
@@ -4,4 +4,16 @@ public class TestSomeThing {
     public void method01(){
         System.out.println("change local -> pull master to local -> add and push -> branch");
     }
+
+    public void method02(){
+        System.out.println("commit part 01");
+    }
+
+    public void method03(){
+        System.out.println("commit part 02");
+    }
+
+    public void method04(){
+        System.out.println("commit part 01");
+    }
 }
